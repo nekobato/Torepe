8822af9276be0626ce214d5e70bf49d61526534b
Hayato Koriyama <nekobato@gmail.com>
2024-01-26 14:51:26 +0900

    refactor: change dir structure

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,17 +6,18 @@ import {
   clipboard,
   Menu,
   dialog,
-} from 'electron';
-import { release } from 'os';
-import { join } from 'path';
-import { checkUpdate } from './autoupdater';
-const isDevelopment = process.env.NODE_ENV !== 'production';
+} from "electron";
+import { release } from "os";
+import { join } from "path";
+import { checkUpdate } from "./autoupdater";
+import { pageRoot, preload, serverUrl } from "./static";
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration();
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName());
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
   app.quit();
@@ -27,104 +28,93 @@ let controllerWindow: BrowserWindow | null;
 let paperWindow: BrowserWindow | null;
 
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } },
+  { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
 
 const updater = checkUpdate();
 
-updater.on('checking-for-update', () => {
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'アップデートのダウンロードが完了しました',
-    message: 'アップデートをインストールしますか？',
-    buttons: ['はい', 'いいえ'],
-  }).then(() => {
-    updater.quitAndInstall();
-  });
+updater.on("checking-for-update", () => {
+  dialog
+    .showMessageBox({
+      type: "info",
+      title: "アップデートのダウンロードが完了しました",
+      message: "アップデートをインストールしますか？",
+      buttons: ["はい", "いいえ"],
+    })
+    .then(() => {
+      updater.quitAndInstall();
+    });
 });
 
-
-const menu = Menu.buildFromTemplate([{ role: 'appMenu' }]);
+const menu = Menu.buildFromTemplate([{ role: "appMenu" }]);
 Menu.setApplicationMenu(menu);
 
 function createWindow() {
   controllerWindow = new BrowserWindow({
-    title: 'Torepe - Controller',
+    title: "Torepe - Controller",
     width: 240,
     height: 440,
     resizable: false,
     webPreferences: {
-      preload: join(__dirname, '../preload/index.cjs'),
+      preload: preload,
       devTools: isDevelopment,
     },
   });
 
   paperWindow = new BrowserWindow({
-    title: 'Torepe - Image',
+    title: "Torepe - Image",
     frame: false,
     hasShadow: false,
     transparent: true,
     resizable: true,
     roundedCorners: false,
     webPreferences: {
-      preload: join(__dirname, '../preload/index.cjs'),
+      preload: preload,
       devTools: isDevelopment,
     },
     show: false,
   });
 
-  let urlPrefix: string;
+  controllerWindow.loadURL(`${pageRoot}#/dropper`);
+  paperWindow.loadURL(`${pageRoot}#/paper`);
 
-  if (process.env.NODE_ENV === 'development') {
-    urlPrefix = 'http://localhost:3000/';
-  } else {
-    urlPrefix = require('url').format({
-      protocol: 'file',
-      slashes: true,
-      pathname: require('path').join(__dirname, '../renderer/index.html'),
-    });
-  }
-
-  controllerWindow.loadURL(`${urlPrefix}#/dropper`);
-  paperWindow.loadURL(`${urlPrefix}#/paper`);
-
-  controllerWindow.on('closed', () => {
+  controllerWindow.on("closed", () => {
     controllerWindow = null;
   });
 
-  paperWindow.on('moved', () => {
+  paperWindow.on("moved", () => {
     if (!controllerWindow || !paperWindow) return;
-    controllerWindow.webContents.send('window-rectangle', {
+    controllerWindow.webContents.send("window-rectangle", {
       ...paperWindow.getBounds(),
       original: false,
     });
   });
 
-  paperWindow.on('will-resize', (_, rectangle) => {
+  paperWindow.on("will-resize", (_, rectangle) => {
     if (!controllerWindow) return;
-    controllerWindow.webContents.send('window-rectangle', {
+    controllerWindow.webContents.send("window-rectangle", {
       ...rectangle,
       original: false,
     });
   });
 
-  ipcMain.on('renderer-event', (_, event: string, payload: any) => {
+  ipcMain.on("renderer-event", (_, event: string, payload: any) => {
     if (!paperWindow || !controllerWindow) return;
     switch (event) {
-      case 'set-opacity':
+      case "set-opacity":
         paperWindow.webContents.send(event, payload);
         break;
-      case 'toggle-clickthrough':
+      case "toggle-clickthrough":
         paperWindow.setIgnoreMouseEvents(payload.toggle);
         paperWindow.setAlwaysOnTop(payload.toggle);
         break;
-      case 'set-bounds':
+      case "set-bounds":
         paperWindow.setBounds(payload);
         break;
-      case 'set-position':
+      case "set-position":
         paperWindow.setBounds(payload);
         break;
-      case 'move-position':
+      case "move-position":
         console.log(payload);
         const bounds = paperWindow.getBounds();
         paperWindow.setBounds({
@@ -133,13 +123,13 @@ function createWindow() {
           y: bounds.y + payload.y,
         });
         break;
-      case 'set-image':
-        if (payload.type === 'clipboard') {
+      case "set-image":
+        if (payload.type === "clipboard") {
           const image = clipboard.readImage();
           if (image.isEmpty()) {
             dialog.showErrorBox(
-              'クリップボードに画像がありません',
-              '画像をコピーしてから再度お試しください'
+              "クリップボードに画像がありません",
+              "画像をコピーしてから再度お試しください"
             );
             return;
           }
@@ -150,11 +140,11 @@ function createWindow() {
         paperWindow.show();
         paperWindow.webContents.send(event, payload);
 
-        controllerWindow.webContents?.send('goto-controller');
+        controllerWindow.webContents?.send("goto-controller");
         break;
-      case 'set-image-size':
+      case "set-image-size":
         paperWindow.setSize(payload.width, payload.height);
-        controllerWindow.webContents.send('window-rectangle', {
+        controllerWindow.webContents.send("window-rectangle", {
           x: paperWindow.getBounds().x,
           y: paperWindow.getBounds().y,
           width: payload.width,
@@ -162,10 +152,10 @@ function createWindow() {
           original: true,
         });
         break;
-      case 'reset-image':
+      case "reset-image":
         paperWindow.hide();
         break;
-      case 'link-aspect':
+      case "link-aspect":
         const { link, ratio } = payload;
         if (link) {
           paperWindow.setAspectRatio(ratio);
@@ -175,39 +165,39 @@ function createWindow() {
     }
   });
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     controllerWindow.webContents.openDevTools();
     paperWindow.webContents.openDevTools();
   }
 
-  controllerWindow.on('closed', () => {
+  controllerWindow.on("closed", () => {
     app.quit();
   });
 }
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   app.quit();
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (controllerWindow === null) {
     createWindow();
   }
 });
 
-app.on('ready', async () => {
+app.on("ready", async () => {
   createWindow();
 });
 
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
         app.quit();
       }
     });
   } else {
-    process.on('SIGTERM', () => {
+    process.on("SIGTERM", () => {
       app.quit();
     });
   }
---
+++ src/main/static.ts
@@ -0,0 +1,18 @@
+import { app } from "electron";
+import path from "node:path";
+
+export const root = path.join(__dirname, "..");
+
+export const mainRoot = path.join(__dirname, "../main");
+export const rendererRoot = path.join(__dirname, "../renderer");
+export const preloadRoot = path.join(__dirname, "../preload");
+export const resourcesRoot = app.isPackaged
+  ? path.join(__dirname, "../resources")
+  : path.join(__dirname, "../resources");
+export const serverUrl = process.env["ELECTRON_RENDERER_URL"];
+
+export const preload = path.join(preloadRoot, "index.js");
+
+export const pageRoot = serverUrl
+  ? (serverUrl as string) // dev
+  : path.join(rendererRoot, "index.html"); // prod
